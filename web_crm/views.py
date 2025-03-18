f9488c2b6ff914a1cf21dec2b2bba293b4cc172a
lakshman nanchari <lakshmannanchari96@gmail.com>
2025-03-18 14:32:32 +0530

    user login

--- web_crm/views.py
+++ web_crm/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    #check to see if logged in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+    #Authenticate user
+        user = authenticate(request, username= username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You Have Been Logged In')
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error Logging In")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+
+
+def logout_user(request):
+    pass 
